b3fc9117ad8e9184e8699b29f5d9878743f6da7a
unknown <sladjana.pajich@gmail.com>
2018-09-23 11:43:16 -0700

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import Notifications from './components/Notifications.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Notifications
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
