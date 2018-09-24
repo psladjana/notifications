25affaa959608b45797aa320f0656d41b9f3962e
unknown <sladjana.pajich@gmail.com>
2018-09-24 00:16:52 -0700

    style corrections

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -1,56 +1,66 @@
 <template>
   <v-menu offset-y left transition="slide-y-transition">
-    <v-icon color="blue accent-2" large>arrow_drop_up</v-icon>
-    <v-btn slot="activator" class="nootificationBtn" color="white" @click="deleteNotification">
+    <v-icon color="blue accent-2" large class="arrow_icon">arrow_drop_up</v-icon>
+    <v-btn slot="activator" class="nootification_btn" color="white" @click="deleteNotification">
       <v-icon color="grey darken-1">notifications</v-icon>
       <v-badge color="red">
         <span slot="badge">{{ unreadNotificationsCount }}</span>
       </v-badge> 
     </v-btn>
 
-    <v-list>
+    <v-list two-line>
       <v-card>
         <v-card-title class="blue accent-2">
           <h3>Notifications</h3>
         </v-card-title>
+        
         <v-list-tile
           avatar
           ripple
           v-for="(item, index) in notifications"
           :key="index"
-          @click=""
         >
         <!-- Rendering of Promotion item -->
-        <template v-if="item.type === 'promotion'">
+        <template v-if="item.type === 'promotion'" transition="slide-y-transition">
           <v-list-tile-avatar>
             <img v-bind:src="item.image" />
           </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
           </v-list-tile-content>
+
         </template>
 
         <!-- Rendering of Bonus item -->
-        <template v-if="item.type === 'bonus'">
+
+        <template v-if="item.type === 'bonus'" transition="slide-y-transition">
+        <v-list-tile-avatar>
+          <v-icon class="blue accent-2 white--text">notifications</v-icon>
+        </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             <v-list-tile-sub-title>{{ item.requirement }}</v-list-tile-sub-title>
           </v-list-tile-content>
+ 
         </template>
 
         <!-- Rendering of Text item -->
-        <template v-if="item.type === 'text'">
+   
+        <template v-if="item.type === 'text'" transition="slide-y-transition">
+        <v-list-tile-avatar>
+          <v-icon class="blue accent-2 white--text">notifications</v-icon>
+        </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title>{{ item.title }}</v-list-tile-title>
             <v-list-tile-sub-title>{{ item.text }}</v-list-tile-sub-title>
           </v-list-tile-content>
+          
         </template>
 
         <!-- Rendering of "new" button -->
-          <v-btn color="success" right top small v-if="!item.seen">New</v-btn> 
-          </v-list-tile>
+          <v-chip color="light-green" text-color="white" v-if="!item.seen">New</v-chip>
+          
         </v-list-tile>
-        
       </v-card>
     </v-list>
 
@@ -61,9 +71,9 @@
   export default {
     data () {
       return {
-      small: true,
-      top: true,
-      right: true
+        small: true,
+        top: true,
+        right: true
       }
     },
     created () {
@@ -88,7 +98,7 @@
   }
 </script>
 <style lang="scss">
-.nootificationBtn {
+.nootification_btn {
   min-width: 30px;
   margin: 20px;
   padding: 0 7px;
@@ -101,7 +111,7 @@
   overflow-y: visible;
   overflow-x: visible;
   margin-top: 20px;
-  .v-icon {
+  .arrow_icon {
       position: absolute;
       top: -21px;
       overflow: visible;
@@ -119,5 +129,24 @@
 }
 .v-list {
   padding: 0;
+  &__tile {
+    border-bottom: 1px solid rgba(0,0,0,0.12);
+    margin: 10px 10px 0;
+    padding-left: 0
+  }
+  .v-chip {
+    border-radius: 7px;
+    margin: 0;
+    position: absolute;
+    right: 0px;
+    top: 0;
+    &__content {
+      font-size: 11px;
+      text-transform: uppercase;
+      height: auto;
+      padding: 2px 10px;
+      line-height: 1
+    }
+  }
 }
 </style>
